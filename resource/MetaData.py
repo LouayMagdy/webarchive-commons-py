397a686f1c1d2af24e8bfd169ad3c6690f0611f4
LouayMagdy <magdylouay@gmail.com>
2023-08-30 09:41:55 +0300

    Metadata class implementation tested

--- resource/MetaData.py
+++ resource/MetaData.py
@@ -3,7 +3,6 @@ import logging
 
 class MetaData(dict):
     _logger = logging.getLogger(__name__)
-    _topMetaData = None
 
     def __init__(self, parent_meta_data=None, name=None):
         super().__init__()
@@ -11,46 +10,23 @@ class MetaData(dict):
             self._topMetaData = self
         else:
             self._topMetaData = parent_meta_data.get_top_meta_data()
-        if name is not None:
-            parent_meta_data.putChild(name, self)
+            if name is not None:
+                parent_meta_data.put(name, self)
 
     def get(self, key):
         try:
-            return super().get(key)
-        except KeyError as e:
-            self._logger.warning(e)
-            return None
-
-    def get_boolean(self, key):
-        try:
-            return super().__getitem__(key)
-        except KeyError as e:
-            self._logger.warning(e)
-            return False
-
-    def get_int(self, key):
-        try:
-            return super().__getitem__(key)
-        except KeyError as e:
-            self._logger.warning(e)
-            return -1
-
-    def get_long(self, key):
-        try:
-            return super().__getitem__(key)
-        except KeyError as e:
-            self._logger.warning(e)
-            return -1
-
-    def get_string(self, key):
-        try:
             return super().__getitem__(key)
         except KeyError as e:
             self._logger.warning(e)
             return None
 
-    def create_child(self, name):
-        return MetaData(self, name)
+    def put(self, key, value):
+        try:
+            super().__setitem__(key, value)
+            return self
+        except KeyError as e:
+            self._logger.warning(e)
+            return None
 
     def get_top_meta_data(self):
         if self._topMetaData is None:
@@ -60,44 +36,15 @@ class MetaData(dict):
     def set_top_meta_data(self, top_meta_data):
         self._topMetaData = top_meta_data
 
-    def put_string(self, key, value):
-        try:
-            super().__setitem__(key, value)
-            return self
-        except KeyError as e:
-            self._logger.warning(e)
-            return None
-
-    def put_long(self, key, value):
-        try:
-            super().__setitem__(key, str(value))
-            return self
-        except KeyError as e:
-            self._logger.warning(e)
-            return None
-
-    def put_boolean(self, key, value):
-        try:
-            super().__setitem__(key, value)
-            return self
-        except KeyError as e:
-            self._logger.warning(e)
-            return None
-
-    def put_child(self, key, child):
-        try:
-            super().__setitem__(key, child)
-            return self
-        except KeyError as e:
-            self._logger.warning(e)
-            return None
+    def create_child(self, name):
+        return MetaData(self, name)
 
     def append_child(self, key, child):
         try:
-            jarray = self.get(key)
-            if jarray is None:
+            jarray = [self.get(key)]
+            if jarray[0] is None:
                 jarray = []
-                self[key] = jarray
+            self.put(key, jarray)
             jarray.append(child)
             return self
         except KeyError as e:
@@ -106,6 +53,7 @@ class MetaData(dict):
 
     def append_obj(self, key, *args):
         n = {}
+        print(key, args, len(args))
         if len(args) % 2 == 1:
             raise ValueError("Odd number of arguments")
         try:
@@ -115,4 +63,16 @@ class MetaData(dict):
         except KeyError as e:
             self._logger.warning(e)
 
-# class MetaDataEncoder:
+
+# metadata = MetaData()
+# metadata.put("first", 1)
+# metadata.put("second", "ok")
+# metadata.put("third", True)
+# print(metadata.get("first"), metadata.get("second"), metadata.get("third"))
+# child1 = metadata.create_child("c1")
+# child2 = metadata.create_child("c2")
+# metadata.append_child("c1", child2)
+# metadata.append_child("c3", child2)
+# print(metadata.get("c1"), metadata.get("c3"))
+# metadata.append_obj("key", "a", 1, "b", 2, "c", False)
+# print(metadata.get("key"))
